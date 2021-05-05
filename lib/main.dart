4981a0bdcaa91cd0b405d5766b71a9617dfb9a00
Hasvingunasekara <hasvingunasekara@gmail.com>
2021-05-05 08:56:24 +0530

    final Commit, All done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:pokemon_app/pokemon.dart';
+import 'package:pokemon_app/pokemondetail.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -45,20 +46,33 @@ class _HomepageState extends State<Homepage> {
         title: Text("Poke App"),
         backgroundColor: Colors.cyan,
       ),
-      body: GridView.count(crossAxisCount: 2, children: pokeHub.pokemon.map((poke) => Padding(
+      body: pokeHub == null? Center(child: CircularProgressIndicator(),) : GridView.count(crossAxisCount: 2, children: pokeHub.pokemon.map((poke) => Padding(
         padding: const EdgeInsets.all(2.0),
-        child: Card(
-          child:Column(
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: NetworkImage(poke.img))
-                ),
-              )
-            ],
-          ) ,),
+        child: InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => PokeDetail(
+              pokemon: poke,
+            )));
+          },
+          child: Hero(
+            tag: poke.img,
+            child: Card(
+              elevation: 3.0,
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(image: NetworkImage(poke.img))
+                    ),
+                  ),
+                  Text(poke.name, style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),)
+                ],
+              ) ,),
+          ),
+        ),
       )).toList(),),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.refresh),backgroundColor: Colors.cyan,),
